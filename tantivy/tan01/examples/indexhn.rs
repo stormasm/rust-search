13f92ae24fbf72cf11e2c67793f57d4b8bc24a58
Michael Angerman <stormasm@gmail.com>
2020-01-17 15:11:14 -0800

    indexhn up and running

--- tantivy/tan01/examples/indexhn.rs
+++ tantivy/tan01/examples/indexhn.rs
@@ -1,3 +1,6 @@
+use std::fs::{create_dir, remove_dir_all};
+use std::path::Path;
+
 use tantivy::schema::Field;
 use tantivy::schema::*;
 use tantivy::{doc, Index};
@@ -12,7 +15,16 @@ fn create_schema() -> Schema {
 fn create_index() -> tantivy::Result<Index> {
     let schema = create_schema();
 
-    let index = Index::create_in_ram(schema);
+    let check_path = Path::new("/tmp/tantivy/idxbs");
+    let dir_exists = check_path.exists();
+    if dir_exists {
+        remove_dir_all(check_path).expect("dir does not exist");
+    }
+
+    let index_path = Path::new("/tmp/tantivy/idxbs");
+    create_dir(index_path).expect("dir already exists");
+
+    let index = Index::create_in_dir(&index_path, schema.clone())?;
 
     let mut index_writer = index.writer_with_num_threads(1, 3_000_000)?;
     let title = index.schema().get_field("title").unwrap();

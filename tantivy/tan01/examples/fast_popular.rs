b483c5ec35a20772b7cff7a7b61df2594e3f38ec
Michael Angerman <stormasm@gmail.com>
2020-01-17 14:49:47 -0800

    fast_popular is up and running

--- tantivy/tan01/examples/fast_popular.rs
+++ tantivy/tan01/examples/fast_popular.rs
@@ -1,40 +1,44 @@
-use tantivy::SegmentReader;
-use tantivy::collector::TopDocs;
 use tantivy::schema::Field;
+use tantivy::SegmentReader;
+
+use tantivy::collector::TopDocs;
+use tantivy::query::QueryParser;
+use tantivy::schema::*;
+use tantivy::{doc, DocAddress, DocId, Index, Score};
 
 fn create_schema() -> Schema {
-   let mut schema_builder = Schema::builder();
-   schema_builder.add_text_field("product_name", TEXT);
-   schema_builder.add_u64_field("popularity", FAST);
-   schema_builder.build()
+    let mut schema_builder = Schema::builder();
+    schema_builder.add_text_field("product_name", TEXT);
+    schema_builder.add_u64_field("popularity", FAST);
+    schema_builder.build()
 }
 
 fn create_index() -> tantivy::Result<Index> {
-  let schema = create_schema();
-  let index = Index::create_in_ram(schema);
-  let mut index_writer = index.writer_with_num_threads(1, 3_000_000)?;
-  let product_name = index.schema().get_field("product_name").unwrap();
-  let popularity: Field = index.schema().get_field("popularity").unwrap();
-  index_writer.add_document(doc!(product_name => "The Diary of Muadib", popularity => 1u64));
-  index_writer.add_document(doc!(product_name => "A Dairy Cow", popularity => 10u64));
-  index_writer.add_document(doc!(product_name => "The Diary of a Young Girl", popularity => 15u64));
-  index_writer.commit()?;
-  Ok(index)
+    let schema = create_schema();
+    let index = Index::create_in_ram(schema);
+    let mut index_writer = index.writer_with_num_threads(1, 3_000_000)?;
+    let product_name = index.schema().get_field("product_name").unwrap();
+    let popularity: Field = index.schema().get_field("popularity").unwrap();
+    index_writer.add_document(doc!(product_name => "The Diary of Muadib", popularity => 1u64));
+    index_writer.add_document(doc!(product_name => "A Dairy Cow", popularity => 10u64));
+    index_writer
+        .add_document(doc!(product_name => "The Diary of a Young Girl", popularity => 15u64));
+    index_writer.commit()?;
+    Ok(index)
 }
 
 fn main() -> tantivy::Result<()> {
-let index = create_index().unwrap();
-let product_name = index.schema().get_field("product_name").unwrap();
-let popularity: Field = index.schema().get_field("popularity").unwrap();
+    let index = create_index().unwrap();
+    let product_name = index.schema().get_field("product_name").unwrap();
+    let popularity: Field = index.schema().get_field("popularity").unwrap();
 
-let user_query_str = "diary";
-let query_parser = QueryParser::for_index(&index, vec![product_name]);
-let query = query_parser.parse_query(user_query_str).unwrap();
+    let user_query_str = "diary";
+    let query_parser = QueryParser::for_index(&index, vec![product_name]);
+    let query = query_parser.parse_query(user_query_str).unwrap();
 
-// This is where we build our collector with our custom score.
-let top_docs_by_custom_score = TopDocs
-        ::with_limit(10)
-         .tweak_score(move |segment_reader: &SegmentReader| {
+    // This is where we build our collector with our custom score.
+    let top_docs_by_custom_score =
+        TopDocs::with_limit(10).tweak_score(move |segment_reader: &SegmentReader| {
             // The argument is a function that returns our scoring
             // function.
             //
@@ -44,8 +48,7 @@ let top_docs_by_custom_score = TopDocs
             //
             // In our case, we will get a reader for the popularity
             // fast field.
-            let popularity_reader =
-                segment_reader.fast_fields().u64(popularity).unwrap();
+            let popularity_reader = segment_reader.fast_fields().u64(popularity).unwrap();
 
             // We can now define our actual scoring function
             move |doc: DocId, original_score: Score| {
@@ -55,12 +58,13 @@ let top_docs_by_custom_score = TopDocs
                 let popularity_boost_score = ((2u64 + popularity) as f32).log2();
                 popularity_boost_score * original_score
             }
-          });
-let reader = index.reader().unwrap();
-let searcher = reader.searcher();
-// ... and here are our documents. Note this is a simple vec.
-// The `Score` in the pair is our tweaked score.
-let resulting_docs: Vec<(Score, DocAddress)> =
-     searcher.search(&query, &top_docs_by_custom_score).unwrap();
-Ok(())
+        });
+    let reader = index.reader().unwrap();
+    let searcher = reader.searcher();
+    // ... and here are our documents. Note this is a simple vec.
+    // The `Score` in the pair is our tweaked score.
+    let resulting_docs: Vec<(Score, DocAddress)> =
+        searcher.search(&query, &top_docs_by_custom_score).unwrap();
+    println!("{:?}", resulting_docs);
+    Ok(())
 }

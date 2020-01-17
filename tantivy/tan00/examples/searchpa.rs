9188ba1ba14f8a9e235da614ab262a61dc7ce4db
Michael Angerman <stormasm@gmail.com>
2020-01-16 16:46:12 -0800

    appears to be working

--- tantivy/tan00/examples/searchpa.rs
+++ tantivy/tan00/examples/searchpa.rs
@@ -7,55 +7,64 @@ use tantivy::schema::{Document, Schema, STORED, TEXT};
 use tantivy::{doc, Index, ReloadPolicy};
 
 fn main() -> tantivy::Result<()> {
-    let check_path = Path::new("/tmp/tantivy/idxbs");
-    let dir_exists = check_path.exists();
-    if dir_exists {
-        remove_dir_all(check_path).expect("dir does not exist");
-    }
-
     let index_path = Path::new("/tmp/tantivy/idxbs");
-    create_dir(index_path).expect("dir already exists");
 
+    /*
+        let dir_exists = check_path.exists();
+        if dir_exists {
+            remove_dir_all(check_path).expect("dir does not exist");
+        }
+
+        let index_path = Path::new("/tmp/tantivy/idxbs");
+        create_dir(index_path).expect("dir already exists");
+    */
     let mut schema_builder = Schema::builder();
     schema_builder.add_text_field("title", TEXT | STORED);
     schema_builder.add_text_field("body", TEXT);
     let schema = schema_builder.build();
 
-    let index = Index::create_in_dir(&index_path, schema.clone())?;
-    let mut index_writer = index.writer(50_000_000)?;
+    //let index = Index::create_in_dir(&index_path, schema.clone())?;
+
+    let index = Index::open_in_dir(&index_path).unwrap(); //ok
+
+    //    let mut index_writer = index.writer(50_000_000)?;
 
     let title = schema.get_field("title").unwrap();
     let body = schema.get_field("body").unwrap();
 
-    let mut old_man_doc = Document::default();
-    old_man_doc.add_text(title, "The Old Man and the Sea");
-    old_man_doc.add_text(
-        body,
-        "He was an old man who fished alone in a skiff in the Gulf Stream and \
-         he had gone eighty-four days now without taking a fish.",
-    );
+    /*
+        let mut old_man_doc = Document::default();
+        old_man_doc.add_text(title, "The Old Man and the Sea");
+        old_man_doc.add_text(
+            body,
+            "He was an old man who fished alone in a skiff in the Gulf Stream and \
+             he had gone eighty-four days now without taking a fish.",
+        );
 
-    index_writer.add_document(old_man_doc);
+        index_writer.add_document(old_man_doc);
 
-    // For convenience, tantivy also comes with a macro to
-    // reduce the boilerplate above.
-    index_writer.add_document(doc!(
-    title => "Of Mice and Men",
-    body => "A few miles south of Soledad, the Salinas River "
-    ));
+        // For convenience, tantivy also comes with a macro to
+        // reduce the boilerplate above.
+        index_writer.add_document(doc!(
+        title => "Of Mice and Men",
+        body => "A few miles south of Soledad, the Salinas River "
+        ));
 
-    index_writer.add_document(doc!(
-    title => "Ralph Emerson",
-    body => "A few miles south of Soledad, the Salinas River"
-    ));
+        index_writer.add_document(doc!(
+        title => "Ralph Emerson",
+        body => "A few miles south of Soledad, the Salinas River"
+        ));
 
-    // Multivalued field just need to be repeated.
-    index_writer.add_document(doc!(
-    title => "Frankenstein",
-    body => "You will rejoice to hear that no disaster has accompanied"
-    ));
+        // Multivalued field just need to be repeated.
+        index_writer.add_document(doc!(
+        title => "Frankenstein",
+        body => "You will rejoice to hear that no disaster has accompanied"
+        ));
 
-    index_writer.commit()?;
+        index_writer.commit()?;
+
+
+    */
 
     let reader = index
         .reader_builder()

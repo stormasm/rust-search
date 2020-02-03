f44046a3f1294dea5242de87d26d8d5685d3e2e1
Michael Angerman <stormasm@gmail.com>
2020-02-03 14:24:15 -0800

    mw

--- tantivy/tan00/examples/schema_fields1.rs
+++ tantivy/tan00/examples/schema_fields1.rs
@@ -15,22 +15,14 @@ fn main() -> tantivy::Result<()> {
     let index = Index::open_in_dir(&directory)?;
     let schema = index.schema();
 
-    //let x1 = schema
-
     let default_fields: Vec<Field> = schema.fields().map(|(f, _)| f).collect::<Vec<_>>();
 
     let _query_parser =
         QueryParser::new(schema.clone(), default_fields, index.tokenizers().clone());
 
-    //println!("{:?}", default_fields);
-
-    println!("{}", "----------------------");
-
     for val in schema.fields() {
         println!("{:?}", val);
     }
-
-    // let myfields: Vec<> = schema.fields().collect();
 
     for (field, field_entry) in schema.fields() {
         println!("{} {}", field.field_id(), field_entry.name());
